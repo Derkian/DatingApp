4aa8c9f9326f177097dae7e4e6e661f2b8382182
Santos, Rodrigo [Audatex - Brazil] <Rodrigo.Santos@audatex.com.br>
2021-06-20 17:38:19 -0300

    End of Section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -7,20 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Dating App';
-  users : any;
+  title = 'Dating App';  
 
-  constructor(private http: HttpClient){}
-  ngOnInit(): void {
-    this.getUsers();
+  constructor(private accountService : AccountService){}
+
+  ngOnInit(): void {    
+    this.setCurrentUser();
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users')
-    .subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);      
-    });
+
+  setCurrentUser(){
+    const user : User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
 }
