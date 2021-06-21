9f142194299aa424d0f728fe00d2fbe1ff599a62
Santos, Rodrigo [Audatex - Brazil] <Rodrigo.Santos@audatex.com.br>
2021-06-21 20:30:55 -0300

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service'; 
 
 @Component({
@@ -10,7 +12,9 @@ export class NavComponent implements OnInit {
 
   model : any = {}
 
-  constructor(public accountService : AccountService) { }
+  constructor(public accountService : AccountService,
+               private router: Router,
+               private toastr: ToastrService) { }
 
   ngOnInit(): void {    
   }
@@ -18,13 +22,15 @@ export class NavComponent implements OnInit {
   login(){
     this.accountService.login(this.model)
     .subscribe(response => {
-      console.log(response);            
+      this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);      
+      this.toastr.error(error.error);
     });
   }
 
   logout(){
     this.accountService.logout();    
+    this.router.navigateByUrl('/');
   }
 }
